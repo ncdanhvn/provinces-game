6f6ad627eb3ba28d00613cfd38e3be7fd9393bd0
Danh <ncdanhvn@gmail.com>
2023-05-31 07:53:00 +0700

    Install Zoom Pan Pinch module

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,16 @@
+import { TransformComponent, TransformWrapper } from "react-zoom-pan-pinch";
+import Map from "./components/Map";
+
 function App() {
-  return <div>Hello World</div>;
+  return (
+    <div style={{ width: "600px", height: "800px", border: "solid 1px black" }}>
+      <TransformWrapper>
+        <TransformComponent>
+          <Map />
+        </TransformComponent>
+      </TransformWrapper>
+    </div>
+  );
 }
 
 export default App;
