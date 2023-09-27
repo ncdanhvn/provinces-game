616e5d28fa5d06a325037eec4bf26a6e93e2bd77
Danh Nguyen <ncdanhvn@gmail.com>
2023-09-27 19:26:26 +0700

    Refactor Modal.css

--- src/components/Modal/QuestionModal.tsx
+++ src/components/Modal/QuestionModal.tsx
@@ -2,10 +2,7 @@ import { useRef } from "react";
 import Modal, { Styles } from "react-modal";
 import "./Modal.css";
 import { MousePosition, WindowDimensions } from "../../interfaces";
-import {
-    QuestionModalHeight,
-    QuestionModalWidth,
-} from "../../data/const";
+import { QuestionModalHeight, QuestionModalWidth } from "../../data/const";
 import useWindowDimensions from "../../hooks/windowDimensions";
 
 Modal.setAppElement("#root");
@@ -36,10 +33,10 @@ const QuestionModal = ({
             shouldCloseOnOverlayClick={true}
             shouldCloseOnEsc={true}
             onAfterOpen={() => answerRef.current?.focus()}
-            className="modal-box"
+            className="modal modal--main"
             overlayClassName="overlay"
         >
-            <h3 className="modal-title">Tỉnh thành nào đây?</h3>
+            <h3 className="modal__question">Tỉnh thành nào đây?</h3>
             <form
                 onSubmit={(e) => {
                     e.preventDefault();
@@ -47,8 +44,8 @@ const QuestionModal = ({
                     closeModal();
                 }}
             >
-                <input ref={answerRef} className="modal-input" />
-                <button type="submit" className="modal-button">
+                <input ref={answerRef} className="modal__input" />
+                <button type="submit" className="modal__button modal__button--question">
                     Ok
                 </button>
             </form>
@@ -65,20 +62,20 @@ const mousePosToModalStyle = (
     // Check if mousePos is null (for the first time when app just start)
     let x = mousePos?.x ?? 0;
     let y = mousePos?.y ?? 0;
-    
+
     // Calculate where to put question modal, left or right, up or down to click point
-    let xSide = -1; // -1 is left side, 0 is right side, 
+    let xSide = -1; // -1 is left side, 0 is right side,
     if (x < windowDim.width / 2) xSide = 0;
     let ySide = -1; // -1 is up side, 0 is down side
     if (y < windowDim.height / 2) ySide = 0;
 
     // Calculate the buffers
-    let xBuffer = 40
-    if (xSide === -1) xBuffer = -50 
-    let yBuffer = 40
-    if (ySide === -1) yBuffer = -50 
-    const top = y + ySide * QuestionModalHeight + yBuffer
-    const left = x + xSide * QuestionModalWidth + xBuffer
+    let xBuffer = 40;
+    if (xSide === -1) xBuffer = -50;
+    let yBuffer = 40;
+    if (ySide === -1) yBuffer = -50;
+    const top = y + ySide * QuestionModalHeight + yBuffer;
+    const left = x + xSide * QuestionModalWidth + xBuffer;
 
     return {
         content: {
