0bce640b619748160092144b26352c72134c30d0
Danh Nguyen <ncdanhvn@gmail.com>
2023-09-28 09:35:58 +0700

    Edit Start Modal style to match the design

--- src/components/Modal/MainModals.tsx
+++ src/components/Modal/MainModals.tsx
@@ -1,3 +1,4 @@
+import start_arrow from '../../assets/start_arrow.svg'
 import Modal from "react-modal";
 
 Modal.setAppElement("#root");
@@ -23,22 +24,22 @@ const StartGameModal = ({ isOpen, onStartGame }: Props) => {
             contentLabel="Start Game Modal"
             shouldCloseOnOverlayClick={false}
             shouldCloseOnEsc={false}
-            className="modal modal--main"
+            className="modal modal--main modal--start"
             overlayClassName="overlay"
         >
             <div className="modal__title">Cách chơi</div>
             <ol className="modal__ol">
-                <li>Chọn một tỉnh, thành trên bản đồ</li>
-                <li>Nhập tên tỉnh, thành đó</li>
+                <li>Chọn một tỉnh thành trên bản đồ</li>
+                <li>Nhập tên tỉnh thành đó</li>
             </ol>
-            <p>
+            <p className="modal__p modal__p--start">
                 Bạn có <span className="modal__highlight">5 phút</span> để hoàn
-                thành càng nhiều tỉnh, thành càng tốt
+                thành càng nhiều tỉnh thành càng tốt
             </p>
-            <div className="modal__button" onClick={onStartGame}>
-                Bắt Đầu
-                <img src="assets/start_arrow.svg" alt="start arow" />
-            </div>
+            <button className="modal__button modal__button--start" onClick={onStartGame}>
+                <span>Bắt Đầu</span>
+                <img src={start_arrow} alt="start arow" className='modal__button__icon'/>
+            </button>
         </Modal>
     );
 };
