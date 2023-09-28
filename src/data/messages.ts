3a16914710eb6031bb60ed2c92b54ae4aff0fb3a
Danh Nguyen <ncdanhvn@gmail.com>
2023-09-28 13:14:42 +0700

    Add reponse to answer messages (both correct and incorrect)

---
+++ src/data/messages.ts
@@ -0,0 +1,15 @@
+export const FirstTimeMessage = "Nhớ gõ Tiếng Việt có dấu bạn nhé"
+
+export const TryAgain = [    
+    'Chưa đúng rồi, tiếc quá!',
+    'Thử lại lần nữa bạn ơi',
+    'Sao lại sai nhỉ?',
+    'Vẫn chưa đúng',    
+]
+
+export const Congratulation = [
+    'Chính xác, cộng thêm cho bạn 1 điểm',
+    'Đúng rồi, Hay lắm',
+    'Chính xác, tiếp tục đi bạn',
+    'Tỉnh này không làm khó được bạn'
+]
