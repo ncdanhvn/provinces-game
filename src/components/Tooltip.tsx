9d2223425568003770b9ff2b1887cd9154fdcbb4
Danh Nguyen <ncdanhvn@gmail.com>
2023-09-30 23:26:22 +0700

    Set style for tooltip

--- src/components/Tooltip.tsx
+++ src/components/Tooltip.tsx
@@ -1,13 +1,18 @@
 import { MapHeight, MapWidth } from "../data/const";
 import useMousePosition from "../hooks/useMousePosition";
 import { MousePosition } from "../interfaces";
+import "./Modal/Modal.css";
 
 const Tooltip = () => {
     const mousePos = useMousePosition();
     return (
         <div
             id="tooltip"
-            style={{ top: `${getMapPos(mousePos).y}px`, left: `${getMapPos(mousePos).x}px` }}
+            className="modal tooltip"
+            style={{
+                top: `${getMapPos(mousePos).y}px`,
+                left: `${getMapPos(mousePos).x}px`,
+            }}
         ></div>
     );
 };
@@ -18,9 +23,9 @@ const getMapPos = (windowPos: MousePosition): MousePosition => {
     const { innerWidth: width, innerHeight: height } = window;
     let mapPosX = Math.floor(windowPos.x - (width - MapWidth) / 2);
     if (mapPosX < 0) mapPosX = 0;
-    mapPosX += 20
+    mapPosX += 10;
     let mapPosY = Math.floor(windowPos.y - (height - MapHeight) / 2);
     if (mapPosY < 0) mapPosY = 0;
-    mapPosY += 20
+    mapPosY += 10;
     return { x: mapPosX, y: mapPosY };
 };
