acf626665ef3e04da4c146054c02d8b0796ffa15
Danh Nguyen <ncdanhvn@gmail.com>
2023-09-30 09:32:22 +0700

    Refactor question modal position, to have fixed X position

--- src/components/Modal/QuestionModal.tsx
+++ src/components/Modal/QuestionModal.tsx
@@ -18,7 +18,6 @@ const QuestionModal = () => {
         answer,
     } = usePlayStateStore();
 
-
     return (
         <Modal
             isOpen={selectedId !== null}
@@ -32,7 +31,7 @@ const QuestionModal = () => {
             overlayClassName="overlay"
         >
             <h3 className="modal__question">Tỉnh thành nào đây?</h3>
-            { retryMessage && <p className="modal__p">{retryMessage}</p>}
+            {retryMessage && <p className="modal__p">{retryMessage}</p>}
             <form
                 onSubmit={(e) => {
                     e.preventDefault();
@@ -59,27 +58,21 @@ const mousePosToModalStyle = (
     windowDim: WindowDimensions
 ): Styles => {
     // Check if mousePos is null (for the first time when app just start)
-    let x = mousePos?.x ?? 0;
     let y = mousePos?.y ?? 0;
 
-    // Calculate where to put question modal, left or right, up or down to click point
-    let xSide = -1; // -1 is left side, 0 is right side,
-    if (x < windowDim.width / 2) xSide = 0;
+    // Calculate where to put question modal, up or down to click point
     let ySide = -1; // -1 is up side, 0 is down side
     if (y < windowDim.height / 2) ySide = 0;
 
     // Calculate the buffers
-    let xBuffer = 40;
-    if (xSide === -1) xBuffer = -50;
-    let yBuffer = 40;
-    if (ySide === -1) yBuffer = -50;
+    let yBuffer = 55;
+    if (ySide === -1) yBuffer = -yBuffer;
     const top = y + ySide * QuestionModalHeight + yBuffer;
-    const left = x + xSide * QuestionModalWidth + xBuffer;
 
     return {
         content: {
             top: `${top}px`,
-            left: `${left}px`,
+            left: "calc(50% - 150px)",
             right: "auto",
             bottom: "auto",
         },
